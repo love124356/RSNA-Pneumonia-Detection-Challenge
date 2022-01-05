317363c48e93c6dec2e2cb49330f72fa40d562a8
love124356 <as463431@gmail.com>
2022-01-05 18:06:36 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
 # RSNA-Pneumonia-Detection-Challenge
+
+This repository gathers the code for RSNA Pneumonia Detection Challenge from the [Kaggle competition](https://www.kaggle.com/c/rsna-pneumonia-detection-challenge/overview).
+
+We use [Faster R-CNN](https://www.kaggle.com/anastasiiaselezen/rsna-pneumonia-detection-challenge)[1], an open competition notebook using Faster R-CNN to implement.
+
+## Reproducing Submission
+We need to do some pre-preparation for training and testing on the stage 2 dataset.
+
+To reproduce my submission without retrainig, do the following steps:
+1. [Requirement](#Requirement)
+2. [Repository Structure](#Repository-Structure)
+3. [Inference](#Inference)
+
+## Hardware
+
+Ubuntu 18.04.5 LTS
+
+Intel® Core™ i7-3770 CPU @ 3.40GHz × 8
+
+GeForce GTX 1080/PCIe/SSE2
+
+## Requirement
+All requirements should be detailed in requirements.txt.
+
+```env
+$ cd RSNA-Pneumonia-Detection-Challenge
+$ conda env create -f environament.yml
+```
+We use VSCode to open train.ipynb or inference.ipynb, and choose the kernel name "rsnatest". Next, Run all the cells.
+
+Maybe you can use Kaggle or Colab to run these codes.
+
+The jpg images can be downloaded from [here](https://www.kaggle.com/sovitrath/rsna-pneumonia-detection-2018?select=input) and put in root.
+The origin .dam can be downloaded from [here](https://www.kaggle.com/c/rsna-pneumonia-detection-challenge/data) and put in root.
+
+## Repository Structure
+
+The repository structure is:
+```
+RSNA-Pneumonia-Detection-Challenge(root)
+  +-- input                         # all files need in this task
+  |   +-- images                    # training data
+  |   +-- samples                   # testing data
+  |   +-- stage_2_train_labels.csv  # training labels
+  +-- stage_2_test_images           # testing .dcm 
+  +-- models                        # model weights
+  +-- calculate_map.py              # ensemble utils
+  +-- ensemble.py                   # reproduce my submission file
+  +-- inference.ipynb               # for testing 
+  +-- train.ipynb                   # for training model
+  +-- environment.yml               # yaml file for establishing the environment
+```
+
+## Training
+
+To train the model, run train.ipynb:
+
+The "FOLD" parameter is for cross validation. Please modify the number from 0~4 to train.
+
+Trained model will be saved as ```models/fasterrcnn_resnet50_fpn_pneumonia_detection_best.pth```
+
+## Inference
+
+Please download [these five model weights]() if you want to reproduce my submission file, and put them in root or the folder you create.
+
+To reproduce my submission file or test the model you trained, run inference.ipynb.
+
+Note that you need to modify MODEL_PATH and the SUBMISSION_PATH for five model weights(SUBMISSION_PATH should not the same for different weights).
+
+Prediction file will be saved as ```root/{SUBMISSION_PATH}```
+
+
+Finally, you can run the following command to ensemble the five .csv (using conda env "rsnatest")[2].
+
+We also provide the [five .csv]().
+
+```py
+python ensemble.py stage_2_test_images output ensemble.csv 0.csv 1.csv 2.csv 3.csv 4.csv
+```
+
+```0.csv 1.csv 2.csv 3.csv 4.csv``` is the csv you create above step.
+
+## Reference
+[1] [Faster R-CNN notebook](https://www.kaggle.com/anastasiiaselezen/rsna-pneumonia-detection-challenge)
+
+[2] [Ensemble Method](https://gist.github.com/raytroop/abbfb31772a5c8797dade81193da16d5)
---
+++ ensemble.py
@@ -0,0 +1,185 @@
+"""
+Ensembling methods for object detection.
+"""
+import argparse
+import os
+import pandas as pd
+import numpy as np
+import pydicom
+import PIL.Image as Image
+
+from tqdm import tqdm
+# from object_detection.utils import visualization_utils as visutil
+from collections import OrderedDict
+from calculate_map import parse_submission, calculate_iou
+
+"""
+Ensemble - find overlapping boxes of the same class and average their positions
+while adding their confidences. Can weigh different detectors with different weights.
+No real learning here, although the weights and iou_thresh can be optimized.
+Input:
+ - dets : List of detections. Each detection is all the output from one detector, and
+          should be a list of boxes, where each box should be on the format
+          [box_x, box_y, box_w, box_h, class, confidence] where box_x and box_y
+          are the center coordinates, box_w and box_h are width and height resp.
+          The values should be floats, except the class which should be an integer.
+ - iou_thresh: Threshold in terms of IOU where two boxes are considered the same,
+               if they also belong to the same class.
+ - weights: A list of weights, describing how much more some detectors should
+            be trusted compared to others. The list should be as long as the
+            number of detections. If this is set to None, then all detectors
+            will be considered equally reliable. The sum of weights does not
+            necessarily have to be 1.
+Output:
+    A list of boxes, on the same format as the input. Confidences are in range 0-1.
+"""
+def ensemble(dets, conf_thresh=0.5, iou_thresh=0.1, weights=None):
+    assert(type(iou_thresh) == float)
+
+    ndets = len(dets)
+
+    if weights is None:
+        w = 1/float(ndets)
+        weights = [w]*ndets
+    else:
+        assert(len(weights) == ndets)
+
+        s = sum(weights)
+        for i in range(len(weights)):
+            weights[i] /= s
+
+    out = list()
+    used = set()
+
+    for idet in range(ndets):
+        det = dets[idet]
+        for box in det:
+            if tuple(box) in used:
+                continue
+
+            used.add(tuple(box))
+            # Search the other detectors for overlapping box of same class
+            found = []
+            for iodet in range(ndets):
+                odet = dets[iodet]
+
+                if odet == det:
+                    continue
+
+                bestbox = None
+                bestiou = iou_thresh
+                for obox in odet:
+                    if not tuple(obox) in used:
+                        # Not already used
+                        if box[4] == obox[4]:
+                            # Same class
+                            iou = calculate_iou(box[:4], obox[:4])
+                            if iou > bestiou:
+                                bestiou = iou
+                                bestbox = obox
+
+                if not bestbox is None:
+                    w = weights[iodet]
+                    found.append((bestbox, w))
+                    used.add(tuple(bestbox))
+
+            # Now we've gone through all other detectors
+            if not found:
+                new_box = list(box)
+                new_box[5] /= ndets
+                if new_box[5] >= conf_thresh:
+                    out.append(new_box)
+            else:
+                allboxes = [(box, weights[idet])]
+                allboxes.extend(found)
+
+                bx = 0.0
+                by = 0.0
+                bw = 0.0
+                bh = 0.0
+                conf = 0.0
+
+                wsum = 0.0
+                for bb in allboxes:
+                    # weight
+                    w = bb[1]
+                    wsum += w
+
+                    # (box_x, box_y, box_w, box_h)
+                    b = bb[0]
+
+                    bx += w*b[0]
+                    by += w*b[1]
+                    bw += w*b[2]
+                    bh += w*b[3]
+                    conf += w*b[5]
+
+                bx /= wsum
+                by /= wsum
+                bw /= wsum
+                bh /= wsum
+
+                new_box = [bx, by, bw, bh, box[4], conf]
+                if new_box[5] >= conf_thresh:
+                    out.append(new_box)
+    return out
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dicom_dir', help='path to the directory with DICOM images')
+    parser.add_argument('out_images_dir', help='path to the directory for the output images')
+    parser.add_argument('ensembled_submission', help='ensembled submission file')
+    parser.add_argument('submissions', nargs='+', help='subsmission files to ensemble')
+    parser.add_argument('--weights', nargs='+', type=float, help='weight for each submission', default=None)
+    parser.add_argument('--conf-thresh', type=float, help='confidence threshold. default 0.5', default=0.5)
+    parser.add_argument('--iou-thresh', type=float, help='iou threshold. default 0.1', default=0.1)
+    args = parser.parse_args()
+
+    if not args.submissions:
+        print('no submissions')
+        exit()
+
+    if not os.path.exists(args.out_images_dir):
+        os.mkdir(args.out_images_dir)
+
+    submissions = OrderedDict([(s, parse_submission(s)) for s in args.submissions])
+    patient_ids = list(submissions[args.submissions[0]].keys())
+
+    submit_dict = {'patientId': [], 'PredictionString': []}
+    for pid in tqdm(patient_ids):
+        dets = []
+        for _, parsed in submissions.items():
+            sub_dets = []
+            for box, score in zip(parsed[pid]['boxes'], parsed[pid]['scores']):
+                sub_dets.append(box + [1, score])   # only one class `1`
+            dets.append(sub_dets)
+        ensembled = ensemble(dets, conf_thresh=args.conf_thresh, iou_thresh=args.iou_thresh, weights=args.weights)
+        print_boxes = []
+        display_boxes = []
+        display_classes = []
+        display_scores = []
+        for e in ensembled:
+            x, y, w, h, c, conf = e
+            print_boxes.append('{0} {1} {2} {3} {4}'.format(conf, x, y, w, h))
+            display_boxes.append([y, x, y + h, x + w])
+            display_classes.append(c)
+            display_scores.append(conf)
+        submit_dict['patientId'].append(pid)
+        submit_dict['PredictionString'].append(' '.join(print_boxes))
+
+        im = pydicom.read_file(os.path.join(args.dicom_dir, pid + '.dcm'))
+        im = im.pixel_array
+        im = np.stack((im, ) * 3, -1)
+        # if display_boxes:
+        #     visutil.visualize_boxes_and_labels_on_image_array(
+        #         im,
+        #         np.array(display_boxes),
+        #         display_classes,
+        #         display_scores,
+        #         {1: {'id': 1, 'name': 'pneumonia'}},
+        #         use_normalized_coordinates=False,
+        #         min_score_thresh=args.conf_thresh,
+        #     )
+        Image.fromarray(im).save(os.path.join(args.out_images_dir, pid + '.jpg'))
+
+    pd.DataFrame(submit_dict).to_csv(args.ensembled_submission, index=False)
